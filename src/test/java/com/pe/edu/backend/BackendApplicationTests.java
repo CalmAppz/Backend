2c21dcebaca475a9b40624a9b7119077b0323bff
Educando23 <u20211g019@upc.edu.pe>
2025-09-30 01:40:24 -0500

    update actual

---
+++ src/test/java/com/pe/edu/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.edu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
